f7a801621ac94801c0a12b4630a8040b03aa7b9c
ryomakawakami <ryomakwkm@gmail.com>
2019-01-03 20:11:02 -0500

    Classes don't work in RobotC

---
+++ old (class)/example.h
@@ -0,0 +1,213 @@
+// Example for a drive base with VEX 393 motors
+// Written for RobotC
+
+// Base loop flow
+// - Motor inputs updated every 20 ms (LOOPTIME)
+// - Normal looping until within 100 ticks of position
+// - Stop .25 seconds after getting to this position
+
+// Timer algorithm based on jmmckinney's PID library
+
+// Drift correction (autoDrive2)
+// - Maintain same encoder values for left and right
+// - Second PID loop in addition to position PID
+// - Keeps difference between left and right close to 0
+// - Position PID is capped at 100
+// - Even at max speed, drift PID can correct drift
+// - Such as left base at 127, right base at 73
+
+// Slew rate
+// - Prevents robot from changing speed too quickly
+// - Change capped at 10 per loop (MAXSTEP)
+
+// Should be added
+// - Timer to break out of loop if running for too long
+
+#define MAXSTEP 10  // Max change per iteration
+#define LOOPTIME 20   // 20 ms, 50 Hz
+
+#include "pidlib.h"
+
+// PID objects with somewhat reasonable constants
+// Only P is used
+// Not sure if constructors work in this scope
+PID basePID(0.5, 0, 0, 0), driftPID(1, 0, 0, 0);
+
+// Sets left base motors
+void driveL(int power) {
+	motor[left] = power;
+}
+
+// Sets right base motors
+void driveR(int power) {
+	motor[right] = power;
+}
+
+// Returns left encoder value
+int getLeftEncoder() {
+	return SensorValue[leftEncoder];
+}
+
+// Returns right encoder value
+int getRightEncoder() {
+	return SensorValue[rightEncoder];
+}
+
+// Clears encoders
+void clearEncoders() {
+	SensorValue[leftEncoder] = 0;
+	SensorValue[rightEncoder] = 0;
+}
+
+// Using only left encoder
+void autoDrive(int target) {
+
+	bool done = false;
+	long closeTime;
+	float driveOut;
+
+	clearEncoders();
+
+	while(!done) {
+		
+		// Calculate motor output
+		driveOut = basePID.calculate(target, getLeftEncoder());
+		
+		// Set motors to output
+		driveL(driveOut);
+		driveR(driveOut);
+
+		// Sleep for set time
+		sleep(LOOPSPEED);
+
+		// Loop ends .25 seconds after left encoder gets within 100 ticks
+		if(abs(target - getLeftEncoder()) > 100) {
+			closeTime = getTime();
+		}
+
+		if((getTime() - closeTime) > 250) {
+			done = true;
+		}
+
+	}
+
+	// Stop motors
+	driveL(0);
+	driveR(0);
+
+}
+
+// Using both encoders to drive straight
+void autoDrive2(int target) {
+
+	bool done = false;
+	long closeTime;
+	float driveOut, driftOut;
+
+	clearEncoders();
+
+	while(!done) {
+		
+		// Calculate motor output
+		driveOut = basePID.calculate(target, (getLeftEncoder()+getRightEncoder())/2);
+		driftOut = basePID.calculate(target, getLeftEncoder() - getRightEncoder());
+
+		// Limit driveOut from -100 to 100
+		if(abs(driveOut) > 100) {
+			driveOut = sgn(driveOut) * 100;
+		}
+
+		// Set motors to output
+		driveL(driveOut - driftOut);
+		driveR(driveOut + driftOut);
+
+		// Sleep for set time
+		sleep(LOOPSPEED);
+
+		// Loop ends .25 seconds after average gets within 100 ticks
+		if(abs(target - (getLeftEncoder()+getRightEncoder())/2) > 100) {
+			closeTime = getTime();
+		}
+
+		if((getTime() - closeTime) > 250) {
+			done = true;
+		}
+
+	}
+
+	// Stop motors
+	driveL(0);
+	driveR(0);
+
+}
+
+// Using both encoders and slew rate control
+void autoDrive3(int target) {
+
+	bool done = false;
+	long closeTime;
+	float driveOut, driftOut;
+	float outL, outR, lastOutL, lastOutR;
+
+	clearEncoders();
+
+	while(!done) {
+		
+		// Calculate motor output
+		driveOut = basePID.calculate(target, (getLeftEncoder()+getRightEncoder())/2);
+		driftOut = basePID.calculate(target, getLeftEncoder() - getRightEncoder());
+
+		// Limit driveOut from -100 to 100
+		if(abs(driveOut) > 100) {
+			driveOut = sgn(driveOut) * 100;
+		}
+
+		// Calculate motor output
+		outL = driveOut - driftOut;
+		outR = driveOut + driftOut;
+
+		// Slew rate limit
+		if(outL - lastOutL > MAXSTEP) {
+			outL = lastOutL + 10;
+		}
+		else if(outL - lastOutL < -MAXSTEP) {
+			outL = lastOutL - 10;
+		}
+
+		if(outR - lastOutR > MAXSTEP) {
+			outR = lastOutR + 10;
+		}
+		else if(outR - lastOutR < -MAXSTEP) {
+			outR = lastOutR - 10;
+		}
+
+		// Set motors to output
+		driveL(outL);
+		driveR(outR);
+
+		// Store output for slew rate
+		lastOutL = outL;
+		lastOutR = outR;
+
+		// Sleep for set time
+		sleep(LOOPSPEED);
+
+		// Loop ends .25 seconds after average gets within 100 ticks
+		if(abs(target - (getLeftEncoder()+getRightEncoder())/2) > 100) {
+			closeTime = getTime();
+		}
+
+		if((getTime() - closeTime) > 250) {
+			done = true;
+		}
+	}
+
+	// Stop motors
+	driveL(0);
+	driveR(0);
+	
+}
+
+void pre_auton() {
+
+}
---
+++ old (class)/pidlib.h
@@ -0,0 +1,100 @@
+#ifndef PIDLIB
+#define PIDLIB
+
+// Returns system time in ms
+long getTime() {
+    return(nPgmTime);
+}
+
+// PID class
+// lastTime stores time of previous calculation
+// kP, kI, kD, kF are constants for PID
+// lastError stores the last value of the error
+// sigma stores the total error
+class PID {
+    public:
+        PID PID(float p, float i, float d, float f);
+        void setConstants(float p, float i, float d, float f);
+        float calculate(float target, float sensorValue, float range = 10000);
+    private:
+        long lastTime;
+        float kP, kI, kD, kF;
+        float lastError;
+        float sigma;
+};
+
+// PID object constructor
+// lastTime set to current time
+// kP, kI, kD, kF set to inputted values
+// lastError, sigma set to 0
+PID::PID(float p, float i, float d, float f) {
+    lastTime = getTime();
+    kP = p;
+    kI = i;
+    kD = d;
+    kF = f;
+    lastError = 0;
+    sigma = 0;
+}
+
+// PID function setConstants
+// Allows user to input kP, kI, kD and initializes other variables
+// lastTime set to current time
+// kP, kI, kD, kF set to inputted values
+// lastError, sigma set to 0
+void PID::setConstants(float p, float i, float d, float f) {
+    lastTime = getTime();
+    kP = p;
+    kI = i;
+    kD = d;
+    kF = f;
+    lastError = 0;
+    sigma = 0;
+}
+
+// PID function calculate
+// Calculates control loop output
+// Integral range default is a large value
+float PID::calculate(float target, float sensorValue, float range = 10000) {
+    // Declare variables
+    float deltaTime, error, derivative, output;
+
+    // Find change in time and store current
+    deltaTime = (float)(getTime() - lastTime) / 1000.0;
+    lastTime = getTime();
+
+    // Calculate error (P)
+    error = target - sensorValue;
+
+    // Calculate sigma (I)
+    sigma += error * deltaTime;
+
+    // Reset sigma if outside of integral range
+    if(abs(error) > range) {
+        sigma = 0;
+    }
+
+    // Also reset if robot shoots over
+    else if(target > 0) {
+        if(error < 0) {
+            sigma = 0;
+        }
+    }
+    else {
+        if(error > 0) {
+            sigma = 0;
+        }
+    }
+
+    // Calculate derivative (D)
+    // Change in value over change in time and store current
+    derivative = (error - lastError) / deltaTime;
+    lastError = error;
+
+    // Calculate output
+    output = kP * error + kI * sigma + kD * derivative + kF * target;
+
+    return output;
+}
+
+#endif
